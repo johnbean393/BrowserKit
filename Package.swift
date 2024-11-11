db18e1fc169c4edb43fc7b9e249e9bb1078438ff
John Bean <113509988+johnbean393@users.noreply.github.com>
2024-11-11 21:30:50 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .package(url: "https://github.com/scinfu/SwiftSoup.git", exact: "2.6.0"),
         .package(url: "https://github.com/SwiftUIX/SwiftUIX.git", branch: "master"),
         .package(url: "https://github.com/vmanot/CorePersistence.git", .revisionItem("38fd5271fa906a2d8395e4b42724142886a3c763")),
-        .package(url: "https://github.com/vmanot/NetworkKit.git", branch: "master"),
+        .package(url: "https://github.com/johnbean393/NetworkKit.git", branch: "master"),
         .package(url: "https://github.com/vmanot/Swallow.git", branch: "master"),
     ],
     targets: [
